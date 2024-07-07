6ad5b6bbbf8104fb0a1767e81ff65de5f1920689
Lucas-S-S-Cunha <lucassasocunha@gmail.com>
2024-07-07 08:16:43 -0300

    Beta Responsive Site

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const menuButton = document.querySelector(".menu")
+const form = document.querySelector(".formulario-fale-conosco")
+//const mascara = document.querySelector(".mascara-formulario")
+
+addEventListener 
+
+function mostrarForm(){
+    form.style.left = "8%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+function esconderForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "hidden"
+}
